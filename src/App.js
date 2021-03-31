ee7dedd75a9f7c74e4296759cab8270a91fce36a
kuvsnmanikant <69764188+kuvsnmanikant@users.noreply.github.com>
2021-03-31 09:42:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Q2 from './components/Question2';
+import Q3 from './components/Question3';
+import Q4 from './components/Question4';
+import Q5 from './components/Time';
+import Q6 from './components/Qusetion6';
+import Q7 from './components/Question7';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Q2/> 
+      <Q3/>
+      <Q4/>
+      <Q5/>
+      <Q6/>
+      */}
+      <Q7/>
+    </div>
+  );
+}
+
+export default App;
